90d29807e8bb6dcf3d02f4808bfb65d9f46de2cd
jofe12 <jofe12@hotmail.com>
2021-05-15 19:38:12 -0500

    SERVICES_VPS

--- install_ioticosgl.sh
+++ install_ioticosgl.sh
@@ -269,8 +269,9 @@ sudo ./install_docker.sh
 sudo rm install_docker.sh
 sudo curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo git clone https://github.com/ioticos/ioticos_god_level_services.git
-sudo mv ioticos_god_level_services ioticos
+sudo apt install git
+sudo git clone https://github.com/jofe12/servives.git
+##sudo mv ioticos_god_level_services ioticos
 
 
 cd ioticos
@@ -296,8 +297,8 @@ sudo sh -c " echo 'EMQX_DEFAULT_USER_PASSWORD=${EMQX_DEFAULT_USER_PASSWORD}' >> 
 sudo sh -c " echo 'EMQX_DEFAULT_APPLICATION_SECRET=${EMQX_DEFAULT_APPLICATION_SECRET}' >> $filename"
 
 
-sudo git clone https://github.com/ioticos/ioticos_god_level_app.git
-sudo mv ioticos_god_level_app  app
+sudo git clone https://github.com/jofe12/app_web.git
+sudo mv app_web  app
 
 cd app
 
